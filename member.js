8f49cdaf78dd757666992a25ccf6ea173c058060
rmoussawi <rmoussawi@gmail.com>
2023-07-24 04:40:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.color = "#D6D6D6";
+    skills.style.color = "#A9A9A9";
+    projects.style.color = "#A9A9A9";
+    contact.style.color = "#A9A9A9";
+    about.style.color = "#A9A9A9";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
